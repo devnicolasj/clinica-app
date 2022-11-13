0e5e1a6e006449835e5513807775c2c5e9b96ab4
Nicolas <contactonicolasj@gmail.com>
2022-11-12 20:35:24 -0500

    update

--- clinica-app/src/App.js
+++ clinica-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
         }}
       >
         <Link to="/">Home</Link> |{" "}
-        <Link to="/login">Logi</Link> |{" "}
+        <Link to="/login">Loginn</Link> |{" "}
         <Link to="/detalle">Detalle</Link> |{" "}
         <Link to="/checkout">Checkout</Link>
       </nav>
