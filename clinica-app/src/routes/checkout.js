87cc4fcc042aca61a5399816038fbe4f05dfd663
Josué Páez Maldonado <113964097+josuepaez24@users.noreply.github.com>
2022-11-13 18:19:47 -0500

    Delete clinica-app directory

--- clinica-app/src/routes/checkout.js
+++
@@ -1,7 +0,0 @@
-export default function Checkout() {
-    return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Checkout</h2>
-        </main>
-    );
-}
--- clinica-app/src/routes/detalle.js
+++
@@ -1,7 +0,0 @@
-export default function Detalle() {
-    return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Detalle</h2>
-        </main>
-    );
-}
