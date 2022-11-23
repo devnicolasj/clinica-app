5f5f73f143a9edba90433591bf69ff0236bbe4a3
Nicolas <contactonicolasj@gmail.com>
2022-11-22 20:32:40 -0500

    Se sube login del marketplace

---
+++ loginin/src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import useContentful from "./useContentful";
+import ServicioClinica from "./ServicioClinica";
+import { Link } from "react-router-dom";
+
+const App = () => {
+  const [loginin, setLoginin] = useState([]);
+  const { getLoginin } = useContentful();
+
+  useEffect(() => {
+    getLoginin().then((response) => response && setLoginin(response));
+  });
+
+  return (
+    
+    <Wrapper>
+      <TitleWrapper>
+        <div align="center"><h1>Marketplace Clínica Imperial</h1>
+          <p>Para más información presione alguno de los siguientes apartados</p>
+          <div>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/detalle">Detalle</Link> |{" "}
+        <Link to="/checkout">Checkout</Link>
+      </nav>
+    </div>
+        </div>
+      </TitleWrapper>
+      {loginin.map((loginin, index) => (
+        <div align="center"><ServicioClinica key={index} loginin={loginin} /></div>
+      ))}
+      
+    </Wrapper>
+    
+    
+  );
+};
+
+export default App;
+
+const Wrapper = styled.div`
+  font-family: "Roboto";
+  margin: 40px;
+
+  display: grid;
+  row-gap: 40px;
+  justify-content: center;
+`;
+
+const TitleWrapper = styled.div`
+  * {
+    margin: 0;
+  }
+
+  display: grid;
+  row-gap: 10px;
+`;
---
+++ loginin/src/ServicioClinica.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const ServicioClinica = ({ loginin, login }) => {
+  return (
+    <Wrapper background={loginin.imagen.file.url}>
+      <TextContainer>
+        <Title>{loginin.nombre}</Title>
+        <Subtitle>{loginin.descripcion}</Subtitle>
+      </TextContainer>
+  </Wrapper>
+  );
+};
+
+export default ServicioClinica;
+
+const Wrapper = styled.div`
+  display: grid;
+  align-items: flex-end;
+  width: 340px;
+  height: 200px;
+  border-radius: 15px;
+  box-shadow: 0px 20px 40px rgba(52, 53, 99, 0.2),
+    0px 1px 3px rgba(0, 0, 0, 0.05);
+  background: ${(props) =>
+    props.background && `url(${props.background}) center no-repeat`};
+  background-size: auto 120%;
+`;
+
+const TextContainer = styled.div`
+  cursor: pointer;
+  display: grid;
+  background: rgba(255, 255, 255, 0.3);
+  padding: 20px;
+  gap: 10px;
+  width: 100%;
+
+  :hover {
+    height: fit-content;
+    width: auto;
+
+    p:last-child {
+      visibility: visible;
+      display: block;
+    }
+  }
+`;
+
+const Title = styled.p`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  margin: 0;
+  font-weight: bold;
+  color: #000000;
+  margin: 0px;
+`;
+
+const Subtitle = styled.p`
+  font-weight: normal;
+  font-size: 12px;
+  font-style: italic;
+  color: rgba(0, 0, 0, 0.7);
+  margin: 0px;
+  visibility: hidden;
+  display: none;
+`;
